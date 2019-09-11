5639226b9790ed57f753b71dc962d0e6d225a323
Ryma <ryma.nait-amara@redalertlabs.com>
2019-09-11 16:00:26 +0200

    My First commit

---
+++ src/resumeData.js
@@ -0,0 +1,162 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/miranaitamara",
+    "name": "Ryma Nait Amara",
+    "role": "FULL-STACK DEVELOPER",
+    "linkedinId":"Ryma NAITAMARA",
+    "skypeid": "racha.amarcha1",
+    "roleDescription": "I am a computer grad student from UPVD with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!) I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ryma-naitamara-a10a27135/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/miranaitamara",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/miranaitamara0706",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Perpinyà Via Domitia",
+        "specialization":"High Performance Calculation, Simulation",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2018",
+        "Achievements":"Master's degree Erasmus+"
+      },
+      {
+        "UniversityName":"University of Béjaia",
+        "specialization":"Networks and Distributed Systems",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2017",
+        "Achievements":"Master's degree"
+      },
+      {
+        "UniversityName":"University of Béjaia",
+        "specialization":"Computer Development",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2016",
+        "Achievements":"Bachelor's degree"
+      }
+      ,
+      {
+        "UniversityName":"IFTGIE of Béjaia",
+        "specialization":"Developer and Administrator JAVA",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2015",
+        "Achievements":"Bachelor's degree"
+      }
+
+    ],
+    "work":[
+      {
+        "CompanyName":"Red Alert Labs",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"September",
+        "YearOfLeaving":"2019",
+        "Achievements":"Designing, creating and testing new products for companie Red Alert Labs"
+      },
+      {
+        "CompanyName":"Red Alert Labs",
+        "specialization":"Full-Stack Developer",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2019",
+        "Achievements":"Platform RAL"
+      },
+      {
+        "CompanyName":"CERIST-UNIV Béjaia",
+        "specialization":"Full-Stack Developer",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2016",
+        "Achievements":"development of an application allowed to points"
+      },
+      {
+        "CompanyName":"SONATRACH",
+        "specialization":"Full-Stack Developer",
+        "MonthOfLeaving":"march",
+        "YearOfLeaving":"2016",
+        "Achievements":"development of a stock management website  "
+      },
+      {
+        "CompanyName":"ENIEM",
+        "specialization":"Full-Stack Developer",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2015",
+        "Achievements":"development of ENIEM website"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      
+      {
+        "skillname":"bootstrap"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"RAL platform",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/ral.PNG"
+      },
+      {
+        "name":"Cerist",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/permis.PNG",
+       
+      },
+      {
+        "name":"Sonatrach",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/sonatrach.PNG",
+        
+      },
+      {
+        "name":"Eniem",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/eniem.PNG"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"The best developer is Ryma ",
+        "name":"Ayman COO of Red Alert Labs"
+      },
+      {
+        "description":"Super Ryma",
+        "name":"Roland CIO of Red Alert Labs "
+      },
+      {
+        "description":"So you are with we forever super ryma",
+        "name":"Isaac Security Analyst of Red Alert Labs "
+      }
+    ]
+  }
+  
+  export default resumeData
