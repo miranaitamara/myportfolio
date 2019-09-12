1b0bb7da795ded38ba075e2e493e61c4782873f2
Ryma <ryma.nait-amara@redalertlabs.com>
2019-09-12 14:20:52 +0200

    Updates

---
+++ precache-manifest.5c5118501511244c935c54237c571c34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6311c3affd0fe28667be",
+    "url": "/MyPortfolio/static/js/runtime~main.6311c3af.js"
+  },
+  {
+    "revision": "eab4a9769d04343e24f1",
+    "url": "/MyPortfolio/static/js/main.eab4a976.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/MyPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "eab4a9769d04343e24f1",
+    "url": "/MyPortfolio/static/css/main.ceb5cd41.chunk.css"
+  },
+  {
+    "revision": "de627f009b88c2b775e97123ea2f3dd0",
+    "url": "/MyPortfolio/index.html"
+  }
+];
